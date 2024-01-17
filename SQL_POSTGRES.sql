5de7befd477bd8b1ab242ea523d4eebb00eb14ed
Juan Camilo Mamian Ruiz <mr.machineman98@gmail.com>
2024-01-17 09:55:59 -0500

    pl/sql postgresql

--- SQL_POSTGRES.sql
+++ SQL_POSTGRES.sql
@@ -100,3 +100,162 @@ select
 	split_part(a.fechanacimiento::varchar, '-', 1) ||
 	' Buena esa chav@' as mensaje
 	from alumnos a;
+
+
+/*Funciones propias como crearlas
+
+	- Las funciones en POSTGRESQL se les conoce como Stored Procedures
+	exmaple:
+	
+	especificamos que recibimos y que entregamos
+	
+	create function miFuncion()
+	returns text
+	
+	as $$ 
+	begin 
+	
+	end
+	$$ language plgpsql;
+	
+	- los $$ declara el cuerpo de la funcion, el begin end va ser la parte funcional,
+	y el lenguaje es lo que vamos a utilizar en este caso pl
+	
+	example
+	
+*/
+create function miFuncion() returns text as $$
+	begin
+		return 'Bienvenido a mi funcion que hice con PostgreSQL';
+	end;
+$$ language plpgsql;
+
+/*Para sobreescribir la funcion usamos replace*/
+create or replace function miFuncion() returns text as $$
+	begin
+		return 'Bienvenido Juan Camilo';
+	end;
+$$ language plpgsql;
+
+/*Funciones con argumentos*/
+create or replace function miFuncion(num1 int) returns int as $$
+	begin
+		return num1 * 2;
+	end;
+$$ language plpgsql;
+
+create or replace function miFuncion1(num1 int) returns double precision as $$
+	begin
+		return num1 / 2 ::double precision;
+	end;
+$$ language plpgsql;
+
+/*
+	Crear una tabla desde una funcion
+*/
+create or replace function creartabla() returns text as $$
+	begin
+	
+		create table mitabla(
+			nombre varchar(45),
+			edad integer,
+			pais varchar(45)
+		);
+		
+		return 'creando tabla...';
+	end;
+$$ language plpgsql;
+/*Creando tabla y condicionar para que no se presente conflicto al crearse mas de una vez*/
+create or replace function creartabla() returns text as $$
+	begin
+		
+		raise notice 'borrando tabla existente ~mitabla~';
+		if exists( select tablename from pg_tables where tablename = 'mitabla') 
+			then
+				drop table mitabla;
+		end if;
+		raise notice 'creando tabla existente ~mitabla~';
+		if not exists( select tablename from pg_tables where tablename = 'mitabla') 
+			then 
+					create table mitabla(
+						nombre varchar(45),
+						edad integer,
+						pais varchar(45)
+					);
+		end if;
+		
+		return 'creando tabla...';
+	end;
+$$ language plpgsql;
+
+/*Creando tabla y condicionar para que no se presente conflicto al crearse mas de una vez, final insertar un row*/
+create or replace function creartabla() returns text as $$
+	declare
+		query text;
+	begin
+		raise notice 'borrando tabla existente ~mitabla~';
+		if exists( select tablename from pg_tables where tablename = 'mitabla') 
+			then
+				query:= 'drop table mitabla;'; 
+				execute query;
+		end if;
+		raise notice 'creando tabla existente ~mitabla~';
+		if not exists( select tablename from pg_tables where tablename = 'mitabla') 
+			then 
+					query:='create table mitabla(
+						nombre varchar(45),
+						edad integer,
+						pais varchar(45)
+					);';
+					execute query;
+		end if;
+		
+		insert into mitabla(nombre, edad, pais)
+			values('Jose', 45, 'cali');
+			
+		raise notice 'insertando datos en ~mitabla~';
+		
+		alter table mitabla add column telefono varchar(12);
+		raise notice 'agregando nueva columna telefono en ~mitabla~';
+		
+		return 'creando tabla...';
+	end;
+$$ language plpgsql;
+
+/* example final */
+create or replace function creartabla() returns text as $$
+	declare
+		query text;
+	begin
+		raise notice 'borrando tabla existente ~mitabla~';
+		if exists( select tablename from pg_tables where tablename = 'mitabla') 
+			then
+				query:= 'drop table mitabla;'; 
+				execute query;
+		end if;
+		raise notice 'creando tabla existente ~mitabla~';
+		if not exists( select tablename from pg_tables where tablename = 'mitabla') 
+			then 
+					query:='create table mitabla(
+						nombre varchar(45),
+						edad integer,
+						pais varchar(45)
+					);';
+					execute query;
+		end if;
+		
+		insert into mitabla(nombre, edad, pais)
+			values('Jose', 45, 'cali');
+			
+		raise notice 'insertando datos en ~mitabla~';
+		
+		alter table mitabla add column telefono varchar(12);
+		raise notice 'agregando nueva columna telefono en ~mitabla~';
+		
+		query:='update mitabla set telefono = ''3172780054'' where nombre = ''Jose'';';
+		execute query;
+		raise notice 'actualizando datos en ~mitabla~';
+		
+		return 'creando tabla...';
+	end;
+$$ language plpgsql;
